1f3dbf5579542c51adb1f80c676ce047bd21bf5b
Slavi Marinov <admin@orbisius.com>
2025-08-08 12:54:35 +0300

    added bg home

---
+++ pages/bg/home.php
@@ -0,0 +1,15 @@
+<h4>Djebel is in development [bg]</h4>
+<div>
+    If you want to be invited to try <strong>Djebel</strong> when it's beta stable, fill out your details below.
+    By filling out your details below, you allow us to notify you about the app [GDPR stuff]
+</div>
+
+<br/>
+
+[djebel-simple-newsletter render_agree=1 auto_focus=1]
+
+<div>
+    <br>
+    In the meantime, you can check
+    <a href="https://djebel.com/cms/">Djebel Vision</a>
+</div>
