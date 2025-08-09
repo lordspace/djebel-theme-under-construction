7de1e87b111253b5c355ec828db3daafd955177f
Slavi Marinov <admin@orbisius.com>
2025-08-09 11:44:31 +0300

    added vision page

---
+++ pages/vision.php
@@ -0,0 +1,55 @@
+<pre>
+======================================================================================
+TL;DR; Let's create a fast app/CMS inspired by WordPress' best features and with a simple plugin and theme structure.
+If you're interested fill out the form: <a href="https://forms.gle/Ku38XS7xB7M1u5D59" target="_blank">https://forms.gle/Ku38XS7xB7M1u5D59</a>
+======================================================================================
+
+WordPress is a great platform and even though it can solve 80% of the web development challenges it may not be a single solution for everything.
+I really love simplicity and minimalism so I decided to build a new open source app that's fast, lean and inspired by some of the awesome WordPress features.
+
+For example: hooks, shortcodes, simple plugin and theme structure
+
+The idea is to have a fast app that can be anything - cms, blog, brochure site, landing pages and that's still simple to use, to develop and very customizable via plugins of course.
+Also it can support multiple backends such as: text files, sqlite, pgsql, mysql etc. Why just limit it to MySQL/MariaDB?
+
+The core CMS would give you these screens.
+- dashboard
+- posts
+- pages
+- media
+- settings
+- themes
+- plugins
+
+If you want comments: install a plugin.
+if you want meta titles install a plugin
+if you want multisite install a plugin
+If you want multiple languages: install a plugin
+if you want admin area .... install a plugin
+
+Things I really like in WordPress
+- Hooks -> it has filters and actions for lots of things
+- Simple plugin and theme structure
+- WP-CLI project - allows managing WP from the command line and using automation.
+- Roles
+- Backward compatibility
+- Shortcodes
+- Great Community
+- WordCamps
+- Local Meetups
+
+Some developers complain about the legacy code, but WordPress is an awesome business tool that solves real problems and lots of them.
+For example, Membership site, ecommerce (with WooCommerce), Blogging and so much more.
+
+The clients want their problems solved at a good price and efficiently, ideally without reinventing the wheel most of the time.
+
+I think because of things above allowed WP to become more than a blog or a CMS, It became a framework and a solid foundation, so others can build on top of.
+
+If you want to participate or help in any way, fill out this form at <a href="https://djebel.com/cms/" target="_blank">https://djebel.com/cms/</a>
+
+P.S. I strongly believe open source and business should go hand in hand and are not mutually exclusive.
+Donations are not a sustainable business model for most projects.
+
+Slavi Marinov,
+Orbisius
+</pre>
