5809f78c56603c439821410cd77a2242ed3c815f
Slavi Marinov <admin@orbisius.com>
2025-08-09 11:58:30 +0300

    added title

---
+++ pages/contact.php
@@ -0,0 +1,19 @@
+<div>
+    <h2>Contact Us</h2>
+    <p>If you want to contact us or would like to sponsor the project, let us know!</p>
+    
+    <div class="contact-links">
+        <h3>Get in Touch</h3>
+        <ul>
+            <li><a href="https://orbisius.com/quick-contact/" target="_blank">Contact Form</a> - Use our quick contact form for inquiries</li>
+            <li><a href="https://twitter.com/orbisius" target="_blank">Follow us on Twitter/X @orbisius</a></li>
+        </ul>
+    </div>
+    
+    <div class="sponsor-info">
+        <h3>Support the Project</h3>
+        <p>Interested in sponsoring this project? We'd love to hear from you! <a href="https://orbisius.com/quick-contact/" target="_blank">Contact us</a> to discuss sponsorship opportunities.</p>
+    </div>
+
+    [djebel-simple-newsletter title="Subscribe to our newsletter" render_agree=1 auto_focus=1]
+</div>
