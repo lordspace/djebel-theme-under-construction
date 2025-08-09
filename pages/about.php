e14259503cc9a8d25ca0b6a73176acc85330183f
Slavi Marinov <admin@orbisius.com>
2025-08-09 12:33:46 +0300

    upd about

--- pages/about.php
+++ pages/about.php
@@ -2,7 +2,10 @@
 Hi!
 <br/><br/>
 I am Svetoslav (Slavi) Marinov is founder of Orbisius and also behind may other products such as qSandbox, WPSandbox, WPDemo etc. <br/>
-
+I have been using WordPress for many years, starting back in 2005 as a user. <br/>
+At the hosting company I worked for we needed to set up a blog.<br/>
+Several years later I started WordPress plugin development business.<br/>
 I have always wanted to create something that's fast and efficient. <br/>
 
+    <img src="images/djebel_founder_slavi_marinov.jpg" alt="svetoslav marinov" title="djebel founder" />
 </div>
