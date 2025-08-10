9887dc346694be5abd37ae103a81cdfcef72fd5f
Slavi Marinov <admin@orbisius.com>
2025-08-10 23:20:37 +0300

    added some filters

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+
+$obj = new QQ_Theme();
+
+Dj_App_Hooks::addFilter('app.core.request.web_path', [ $obj, 'updateWebPath' ]);
+Dj_App_Hooks::addFilter('app.themes.current_page', [ $obj, 'maybeAddLangPrefix' ]);
+
+class QQ_Theme {
+    public function updateWebPath($web_path)
+    {
+        if (!preg_match('#/(en|bg)$#si', $web_path)) {
+            $web_path .= '/en';
+        }
+
+        return $web_path;
+    }
+    public function maybeAddLangPrefix($page)
+    {
+        if (!preg_match('#^/(en|bg)#si', $page, $matches)) {
+            $page = 'en/' . $page;
+        }
+
+        return $page;
+    }
+}
